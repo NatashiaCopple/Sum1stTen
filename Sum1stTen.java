5ebbf7d9c3a908ac601210a1488ca178f32340c4
Natashia Copple <natashiacopple@gmail.com>
2017-05-06 00:56:58 -0700

    Add files via upload

---
+++ Sum1stTen.java
@@ -0,0 +1,9 @@
+public class Sum1stTen
+{
+    public static void main(String[] args) 
+    {
+        // E1.2
+        int sum = +1+2+3+4+5+6+7+8+9+10;
+        System.out.println("The sum of the first 10 positive integers is: " + sum);
+    }
+}
